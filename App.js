44f8c6ec11015531df51281ace310db6fc021529
PrernaYabaji-10 <116074352+PrernaYabaji-10@users.noreply.github.com>
2023-04-30 20:15:59 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/users" element={<Users/>} />
+      </Routes>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
